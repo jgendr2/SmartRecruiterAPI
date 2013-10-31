f362b267051a5a47552b209c903eb8853d1e4323
Bruce Ackerman <bcackerman@gmail.com>
2013-10-31 16:32:12 -0500

    associative object created, time to send everything to the view

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 SmartRecruitor JS API
 
 All references to SmartRecruitor belong to them, them me.
+
+Getting started locally
+1.  To launch this locally use and run using MAMP/WAML (any local web server)
+2.  /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --disable-web-security &
--- js/core.js
+++ js/core.js
@@ -1,14 +1,12 @@
-// SmartRecruitor (SR) JS API.  All trademarks belong to SmartRecruitor
-// To launch this locally use and run using MAMP/WAML (any local web server)
-// /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --disable-web-security &
 
-srUrl = "http://www.smartrecruiters.com/cgi-bin/WebObjects/share.woa/wa/careersite"
-
-// Set these two variables
-srCompanyName = "AvantCredit1"
-srInstalledURL = "http://avantcredit.com/about_us"
-
+// get all jobs from your SR account
 function getJobs(url) {
+  srUrl = "http://www.smartrecruiters.com/cgi-bin/WebObjects/share.woa/wa/careersite";
+
+  // SET THESE TWO VARIABLES
+  srCompanyName = "AvantCredit1";
+  srInstalledURL = "http://avantcredit.com/about_us";
+
   return $.ajax({
     url: srUrl,
     type: "GET",
@@ -18,38 +16,52 @@ function getJobs(url) {
   });
 }
 
+// get an array of departments you have listed
 function getDepartments(data) {
   if (data.jobs.job[0]) {
     return data.jobs.job[0].company.company_departments.department;
   }
 } 
 
-$(function(){
+// create an associative object of deparments 
+function setAssociativeDeparments(departmentsArray) {
+  var obj = {}
+  $.each(departmentsArray, function(index, value) {
+    obj[value] = [];
+  });
+  return obj;
+}
 
+// create the associative object with jobs in it now (it already had departments)
+function groupJobsInDepartments(departmentsObj, result) {
+  // go through each job
+  // add it to the associative department obj
+  $.map(result, function(value, index) {
+    if (value.department != "") {
+      departmentsObj[value.department].push(value);
+    }
+  });
+  return departmentsObj;
+}
+
+$(function(){
   var promise = getJobs(srUrl);
   var el = $('#list-jobs');
 
   promise.success(function (data) {
     var result = $.xml2json(data);
 
-    $.each(result.jobs.job, function(index, value) {
-      el.append($('<li>' + value.title + '</li>'));
-    });
+    // set hash with departments
+    var departmentsObj = setAssociativeDeparments(getDepartments(result));
+    var groupedJobs = groupJobsInDepartments(departmentsObj, result.jobs.job);
+    
+    // $.each(groupedJobs, function(index, value) {
+    //   el.append($('<li>' + value.title + '</li>'));
+    // });
   });
 
   promise.error(function (e) {
     alert(e.message);
   });
-})
-
-// http://www.smartrecruiters.com/cgi-bin/WebObjects/share.woa/wa/careersite?wpp_company=AvantCredit1&installed_url=http://avantcredit.com/about_us
-
-// input variables for company name and URL where this plugin is placed
-// Grab XML result from a GET
-// Convert to JSON
-// $.each through each of the results
-
-// then refactor to pass a param to group by department
-
-
+});
 
